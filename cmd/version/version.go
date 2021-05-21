e9ad399d264d316a1ca1bdef4558bf2fd6d449b9
marcinromaszewicz <marcinromaszewicz@deepmap.ai>
2021-05-21 13:19:07 -0700

    Add version printer

---
+++ cmd/version/version.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"runtime/debug"
+)
+
+func main() {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		log.Fatalln("could not read build info")
+	}
+	log.Println("Path:", bi.Main.Path)
+	log.Println("Sum:", bi.Main.Sum)
+	log.Println("Version:", bi.Main.Version)
+}
